e5a26ccddee686cd02a08175f15d7b661094feb6
JoseMMedranoJr <jose.m.medranojr+port@gmail.com>
2025-11-12 20:51:52 -0600

    Da Final Solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,12 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(i):
 
-bottle_song()
+	while i > 0:
+		j = i - 1
+		if i == 1:
+			print('1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall')	
+		else:
+			print(f'{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down and pass it around, {j} bottles of beer on the wall.{j} of bottles')
+		i = j
 
-####kldjsfalkfdajalskdfjdfls
+		
+bottle_song(99)
